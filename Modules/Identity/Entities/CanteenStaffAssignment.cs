799d7815f49629e7f6cabf3034da85f017df2943
Dinoroy11 <sentheesdinoshan2006@gmail.com>
2026-08-27 11:01:57 +0530

    feat: add identity entities

--- Modules/Identity/Entities/CanteenStaffAssignment.cs
+++ Modules/Identity/Entities/CanteenStaffAssignment.cs
@@ -1,6 +1,13 @@
-﻿namespace CampusServicePortal_TicUnicorns.Modules.Identity.Entities
+﻿namespace CampusServicePortal.Modules.Identity.Entities;
+
+public class CanteenStaffAssignment
 {
-    public class CanteenStaffAssignment
-    {
-    }
-}
+    public int CanteenStaffAssignmentId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int CanteenId { get; set; }
+
+    // Navigation property
+    public User User { get; set; } = null!;
+} 
--- Modules/Identity/Entities/HostelStaffAssignment.cs
+++ Modules/Identity/Entities/HostelStaffAssignment.cs
@@ -1,6 +1,13 @@
-﻿namespace CampusServicePortal_TicUnicorns.Modules.Identity.Entities
+﻿namespace CampusServicePortal.Modules.Identity.Entities;
+
+public class HostelStaffAssignment
 {
-    public class HostelStaffAssignment
-    {
-    }
+    public int HostelStaffAssignmentId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int HostelId { get; set; }
+
+    // Navigation property
+    public User User { get; set; } = null!;
 }
