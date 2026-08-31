bdccc7a797d0a5884580cabd376a00cbb93710a9
UT011627 <UT011627@UNICOMTIC71>
2026-08-31 13:52:56 +0530

    Implement Gym module

---
+++ Modules/Gym/DTOs/CreateGymSlotDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CampusServicePortal.Modules.Gym.DTOs
+{
+    public class CreateGymSlotDto
+    {
+        public int GymId { get; set; }
+
+        public DateTime SlotDate { get; set; }
+
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan EndTime { get; set; }
+
+        public int MaxCapacity { get; set; }
+    }
+}
---
+++ Modules/Gym/Entities/Gym.cs
@@ -0,0 +1,20 @@
+﻿namespace CampusServicePortal.Modules.Gym.Entities
+{
+    public class Gym
+    {
+        public int GymId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Location { get; set; } = string.Empty;
+
+        public int Capacity { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<GymSlot> GymSlots { get; set; }
+            = new List<GymSlot>();
+    }
+}
