69f3e06a05eafbf8c18cf8f489d3fca03b8fb875
Ragunathan Tharshan <UT011265@unicomtic.com>
2026-09-01 14:11:29 +0530

    Complete Laundry module by Tharshan

---
+++ Modules/Laundry/DTOs/CreateLaundryDto.cs
@@ -0,0 +1,13 @@
+﻿namespace CampusServicePortal_TicUnicorns.Modules.Laundry.DTOs
+{
+    public class CreateLaundryDto
+    {
+        public int StudentId { get; set; }
+
+        public string ServiceType { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public string PickupMethod { get; set; } = string.Empty;
+    }
+}
---
+++ Modules/Laundry/DTOs/UpdateLaundryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CampusServicePortal_TicUnicorns.Modules.Laundry.DTOs
+{
+    public class UpdateLaundryDto
+    {
+        public string ServiceType { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public string PickupMethod { get; set; } = string.Empty;
+    }
+}
