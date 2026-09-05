2979340beafe6e94107e398e8341a7e596994e41
Sanojan Satheeskumar <UT011414@unicomtic.com>
2026-09-05 15:59:25 +0530

    feat: add hostel master data CRUD

---
+++ Modules/Hostels/DTOs/UpdateHostelDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CampusServicePortal.Modules.Hostels.DTOs;
+
+public class UpdateHostelDto
+{
+    public string HostelName { get; set; } = string.Empty;
+
+    public string HostelType { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; }
+}
