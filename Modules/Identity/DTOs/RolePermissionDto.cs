d4e4ee9012f1050363a9b494242897cc4a2be12d
Dinoroy11 <sentheesdinoshan2006@gmail.com>
2026-09-05 14:10:17 +0530

    feat: implement identity service and controller

---
+++ Modules/Identity/DTOs/RolePermissionDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CampusServicePortal.Modules.Identity.DTOs;
+
+public class RolePermissionDto
+{
+    public int RolePermissionId { get; set; }
+
+    public int RoleId { get; set; }
+
+    public int PermissionId { get; set; }
+}
---
+++ Modules/Identity/Repositories/PermissionRepository.cs
@@ -0,0 +1,43 @@
+﻿using CampusServicePortal.Modules.Identity.Entities;
+using CampusServicePortal.Modules.Identity.Interfaces.Repository;
+
+
+namespace CampusServicePortal_TicUnicorns.Modules.Identity.Repositories;
+
+public class PermissionRepository : IPermissionRepository
+{
+    public Task<Permission?> GetByIdAsync(int permissionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Permission?> GetByCodeAsync(string code)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Permission>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Permission permission)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Permission permission)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsAsync(int permissionId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> ExistsByCodeAsync(string code)
+    {
+        throw new NotImplementedException();
+    }
+}
