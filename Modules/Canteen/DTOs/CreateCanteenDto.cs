96c7057691a94b00d76df0f78da38f355e922096
Ragunathan Tharshan <UT011265@unicomtic.com>
2026-09-07 15:34:29 +0530

    Complete Canteen module by Tharshan

---
+++ Modules/Canteen/DTOs/CreateCanteenDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CampusServicePortal_TicUnicorns.Modules.Canteen.DTOs
+{
+    public class CreateCanteenDto
+    {
+        public int StudentId { get; set; }
+
+        public string FoodName { get; set; } = string.Empty;
+
+        public string MealType { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string OrderType { get; set; } = string.Empty;
+    }
+}
---
+++ Modules/Canteen/Interfaces/Repository/ICanteenRepository.cs
@@ -0,0 +1,24 @@
+﻿using CampusServicePortal_TicUnicorns.Modules.Canteen.Entities;
+
+
+namespace CampusServicePortal_TicUnicorns.Modules.Canteens.Interfaces.Repository
+{
+    public interface ICanteenRepository
+    {
+        Task<IEnumerable<CanteenEntities>> GetAllAsync();
+
+        Task<CanteenEntities?> GetByIdAsync(int canteenId);
+
+        Task<IEnumerable<CanteenEntities>> GetByStudentIdAsync(int studentId);
+
+        Task<CanteenEntities> CreateAsync(CanteenEntities canteen);
+
+        Task<CanteenEntities?> UpdateAsync(int canteenId, CanteenEntities canteen);
+
+        Task<CanteenEntities?> UpdateStatusAsync(
+            int canteenId,
+            string status);
+
+        Task<bool> DeleteAsync(int canteenId);
+    }
+}
