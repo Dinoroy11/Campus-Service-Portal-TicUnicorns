858062dfa067aa829552bda705477c52760586c0
Ragunathan Tharshan <UT011265@unicomtic.com>
2026-09-08 11:24:48 +0530

    Add laundry validators by Tharshan

---
+++ Modules/Laundry/Validators/CreateLaundryValidator.cs
@@ -0,0 +1,37 @@
+﻿using CampusServicePortal_TicUnicorns.Modules.Laundry.DTOs;
+using FluentValidation;
+
+namespace CampusServicePortal_TicUnicorns.Modules.Laundry.Validators
+{
+    public class CreateLaundryValidator : AbstractValidator<CreateLaundryDto>
+    {
+        public CreateLaundryValidator()
+        {
+            RuleFor(x => x.StudentId)
+                .GreaterThan(0)
+                .WithMessage("Student ID must be greater than 0.");
+
+            RuleFor(x => x.ServiceType)
+                .NotEmpty()
+                .WithMessage("Service type is required.")
+                .Must(IsValidServiceType)
+                .WithMessage("Invalid laundry service type.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0)
+                .WithMessage("Quantity must be greater than 0.");
+
+            RuleFor(x => x.PickupMethod)
+                .NotEmpty()
+                .WithMessage("Pickup method is required.");
+        }
+
+        private bool IsValidServiceType(string serviceType)
+        {
+            return Enum.TryParse<Enums.LaundryServiceType>(
+                serviceType,
+                true,
+                out _);
+        }
+    }
+}
